09157983133be3e56e61783924067cac8a403779
Javori Smart <jsmart1993@hotmail.com>
2020-05-04 12:20:11 -0400

    finished first two problems

---
+++ recursion-practice.js
@@ -0,0 +1,49 @@
+/* Write a recursive function that counts how many sheep jump over the fence. 
+Your program should take a number as input. 
+That number should be the number of sheep you have. 
+The function should display the number along with the message 
+"Another sheep jumps over the fence" until no more sheep are left.
+
+Input: 3
+Output:
+3: Another sheep jumps over the fence
+2: Another sheep jumps over the fence
+1: Another sheep jumps over the fence
+All sheep jumped over the fence */
+
+// const sheepCounter = function(num) {
+//   if(num === 0) {
+//     console.log('All the sheep has jumped over the fence')
+//     return;
+//   }
+//   else {
+//     console.log(`${num}: Another sheep jumps over the fence`)
+//     return sheepCounter(num - 1);
+//   }
+// }
+
+// sheepCounter(4);
+
+/* Write a function called powerCalculator() 
+that takes two parameters, an integer as a base, 
+and another integer as an exponent. 
+The function returns the value of the base raised to the power of the exponent. 
+Use only exponents greater than or equal to 0 (positive numbers)
+
+powerCalculator(10,2) should return 100
+powerCalculator(10,-2) should return exponent should be >= 0 */
+
+const powerCalculator = function(base, exponent) {
+  if(exponent === 0) {
+    return 1;
+  }
+  else if(exponent <= 0) {
+    return 'Exponent should be >= 0'
+  }
+  else {
+    return base * powerCalculator(base, exponent - 1);
+  }
+
+}
+
+console.log(powerCalculator(3, 4));
